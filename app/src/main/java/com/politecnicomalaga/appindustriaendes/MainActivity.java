11ca5239b205c6d82b295336258c3d9d08dd0659
Andrés Alcaraz Rey <nomail@nomail.com>
2024-04-15 14:14:48 +0200

    Proyecto mara saray

---
+++ app/src/main/java/com/politecnicomalaga/appindustriaendes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.politecnicomalaga.appindustriaendes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
